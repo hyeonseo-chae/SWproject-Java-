bf3678826bee094d232633f1c4b62a98c773f151
unknown <eric71804122@gmail.com>
2022-06-11 01:24:59 +0900

    d

--- Bookmark/src/AddWindow.java
+++ Bookmark/src/AddWindow.java
@@ -15,16 +15,31 @@ public class AddWindow extends JFrame{
 		frame.setPreferredSize(dim);
 		
 		String header[]= {"Group","Name","URL","Memo"};
-		String[][] contents = new String[1][4];
+		String[][] contents = new String[0][4];
 		JTable table = new JTable(contents,header);
-		JScrollPane scrollpane = new JScrollPane(table);
+
 		
+		JScrollPane scrollpane = new JScrollPane(table);
 		button1 = new JButton("INPUT");
 		JPanel rightpanel = new JPanel(new GridLayout(1,1,0,0));
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel,BoxLayout.X_AXIS));
+		
+		JTextField group = new JTextField(3);
+		JTextField name = new JTextField(3);
+		JTextField url = new JTextField(3);
+		JTextField memo = new JTextField(3);
+		
+		panel.add(group);
+		panel.add(name);
+		panel.add(url);
+		panel.add(memo);
 		
 		rightpanel.add(button1);
 		frame.add("East",rightpanel);
 		frame.add("Center",scrollpane);
+		frame.add(panel,BorderLayout.SOUTH);
 		frame.pack();
 		frame.setVisible(true);
 	}
